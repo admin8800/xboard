c433c252220811e696c2385fd6c730d7fce3c5e0
admin8800 <153331261+admin8800@users.noreply.github.com>
2026-05-23 10:15:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ mkdir -p \
   /var/log/supervisor \
   /www/storage/app \
   /www/storage/logs \
+  /www/sqlite-data \
   /www/bootstrap/cache
 
-chown -R www:www /www/storage /www/bootstrap/cache
+chown -R www:www /www/storage /www/bootstrap/cache /www/sqlite-data
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
